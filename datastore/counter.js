478d513f3388220db649c59720dc62b544984311
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-14 15:25:57 -0700

    Complete getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,11 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (cb) => {
+  readCounter((err, id) => {
+    writeCounter( id + 1, (err, id) => cb(id) );
+  });
+
 };
 
 
